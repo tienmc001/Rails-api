37ed8eee8f0eb24aa895336d50515810b2bc832d
Nguyen Minh Tien <156668301+tienmc001@users.noreply.github.com>
2024-03-12 11:08:37 +0700

    setup db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Tạo Company
+10.times { Company.create!(name: Faker::Company.name) }
+
+# Tạo Employee
+50.times {
+  company = Company.all.sample
+  Employee.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    company: company
+  )
+}
